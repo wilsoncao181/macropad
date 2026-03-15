f055bb5fc90e27ba606c79b2e2a5198a3ed66d94
wilsoncao <wilson_cao@icloud.com>
2026-03-15 23:02:30 +0000

    Add files via upload

---
+++ Firmware/keymaps/default/keymap.c
@@ -0,0 +1,26 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[0] = LAYOUT(
+    KC_COPY,
+    KC_PASTE,
+    KC_CUT,
+    KC_UNDO,
+    KC_REDO,
+    KC_MUTE
+)
+
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+    return true;
+}
